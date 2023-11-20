8d0d4c588c34e6e14ba0de3ed0e79d0d22f00165
1115yiming <yimingzhuzhu@gmail.com>
2023-11-20 14:31:58 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// import express from 'express';
+// import HelloRoutes from "./hello.js";
+// import Lab5 from "./lab5.js";
+//
+// //const express = require('express')
+// const app = express()
+//
+// app.listen(4000)
+//
+// HelloRoutes(app);
+// Lab5(app);
+
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import cors from "cors";
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+CourseRoutes(app);
+app.use(express.json());
+ModuleRoutes(app);
+
+Lab5(app);
+Hello(app);
+
+app.listen(process.env.PORT || 4000);
